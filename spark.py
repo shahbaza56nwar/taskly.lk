c345cf4880583e7aa95ef660de89431b1d09f410
shahbaza56nwar <149785360+shahbaza56nwar@users.noreply.github.com>
2023-11-03 17:11:18 +0530

    Update spark.py

--- spark.py
+++ spark.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 
 def run_spark_job():
-    spark = SparkSession.builder.appName("MySparkApp").getOrCreate()
+    spark = SparkSession.builder.appName("Taskly.lk Spark App").getOrCreate()
     df = spark.read.csv("data/sample_data.csv", header=True, inferSchema=True)
     summary = df.describe().toPandas()
     spark.stop()
